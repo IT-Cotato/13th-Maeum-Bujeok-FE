641bbe1e40af00dd99cf99338ff5619d9ec54db9
metjyc <metjyc@naver.com>
2026-07-07 14:04:29 +0900

    chore: paddingtop 사이즈 조절

--- src/features/home/components/HomePage.tsx
+++ src/features/home/components/HomePage.tsx
@@ -8,7 +8,7 @@ import WriteDiaryButton from "@/features/home/components/WriteDiaryButton";
 export default function HomePage() {
   return (
     <main className="min-h-dvh bg-gray-100 text-foreground">
-      <div className="relative mx-auto flex min-h-dvh w-full max-w-[395px] flex-col overflow-hidden bg-background px-6 pb-[calc(126px+env(safe-area-inset-bottom))] pt-[62px]">
+      <div className="relative mx-auto flex min-h-dvh w-full max-w-[395px] flex-col overflow-hidden bg-background px-6 pb-[calc(126px+env(safe-area-inset-bottom))] pt-[28px]">
         <h1 className="text-center text-xl font-medium leading-[23px] text-foreground">
           홈
         </h1>
