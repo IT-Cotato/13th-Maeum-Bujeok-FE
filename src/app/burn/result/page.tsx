0db378ec7104e186404e5e731f4a8dff9bb82f48
metjyc <metjyc@naver.com>
2026-07-09 10:01:53 +0900

    chore: 소각 결과화면 연결

--- src/app/burn/result/page.tsx
+++ src/app/burn/result/page.tsx
@@ -1,13 +1,5 @@
-import PlaceholderScreen from "@/components/common/PlaceholderScreen";
+import BurnResultPage from "@/features/burn/components/BurnResultPage";
 
-export default function BurnResultPage() {
-  return (
-    <PlaceholderScreen
-      activeValue="burn"
-      description="소각 결과 화면은 다음 단계에서 연결될 예정이에요."
-      eyebrow="소각 완료"
-      showBackLink
-      title="소각하기"
-    />
-  );
+export default function Page() {
+  return <BurnResultPage />;
 }
